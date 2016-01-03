9699e92a4cdfe4d2498db9ff01e78cce97e24496
Erik Waibel <erik.waibel@gm.com>
2016-01-03 16:48:11 -0700

    Initial commit of JavaScript for puzzle code

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
-(function($) {
+var Quotefall = function(quote) {
+	this.quote = quote;
+	this.letters = quote.split('');
+	this.totalColumns = Math.ceil(this.letters.length / this.rows);
+};
 
+Quotefall.prototype = {
+	rows: 4
+};
+
+(function($) {
+	var quote = 'if those space scientists are so smart why do they all count backward',
+		puzzle = new Quotefall(quote);
 })(jQuery);
