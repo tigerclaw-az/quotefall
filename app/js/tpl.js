87c8fe8a2c68f3c0e836164c4f60ebd5b27e82eb
Erik Waibel <erik.waibel@gm.com>
2016-01-03 16:47:20 -0700

    Remove 'js/tpl.js' and add 'js/lib' to .gitignore

--- app/js/tpl.js
+++
@@ -1,1 +0,0 @@
-(function(dust){dust.register("puzzle.html",body_0);function body_0(chk,ctx){return chk.w("<div id=\"test\"><span>T</span><span>T</span><span>A</span><span>T</span></div>");}body_0.__dustBody=!0;return body_0}(dust));
