48c31b0492850dd788404f4393b7a095a590a3f5
Erik Waibel <erik.waibel@gm.com>
2017-05-16 23:24:41 -0700

    Move 'styles' tasks into new gulp file styles.js

--- gulp/build.js
+++ gulp/build.js
@@ -16,34 +16,6 @@ var conf = require('./config'),
 	options = conf.options,
 	paths = conf.paths;
 
-gulp.task('styles:lint', function() {
-	return gulp.src(paths.styles.source.all)
-		.pipe($.plumber())
-		.pipe($.sassLint(options.sasslint))
-		.pipe($.sassLint.format())
-		.pipe($.sassLint.failOnError());
-});
-
-gulp.task('styles', ['styles:lint'], function() {
-	var injectFiles = gulp.src(paths.styles.source.inject, { read: false }),
-		postcssPlugins = [
-			$.autoprefixer(options.autoprefixer)
-		];
-
-	return gulp.src(paths.styles.source.index)
-		.pipe($.plumber(options.errorHandler))
-		.pipe($.inject(injectFiles, options.inject)).on('error', conf.errorHandler('inject'))
-		.pipe(wiredep(options.wiredep))
-		.pipe($.sourcemaps.init())
-		.pipe($.sass(options.sass)).on('error', conf.errorHandler('sass'))
-		.pipe($.postcss(postcssPlugins)).on('error', conf.errorHandler('postcss'))
-		.pipe($.sourcemaps.write())
-		.pipe($.rename('main.css'))
-		.pipe(gulp.dest(paths.styles.serve))
-		.pipe(browserSync.reload({ stream: true }))
-		.pipe($.size());
-});
-
 gulp.task('partials', function() {
 	return gulp.src([
 		path.join(paths.app, '/**/*.html'),
