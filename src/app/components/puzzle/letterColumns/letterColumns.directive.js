a7a7f61548a148d54099db984e8d951419aefc54
Erik Waibel <erik.waibel@gm.com>
2017-06-02 14:39:45 -0700

    Update letterColumns to allow user interaction and add appropriate classes

--- src/app/components/puzzle/letterColumns/letterColumns.directive.js
+++ src/app/components/puzzle/letterColumns/letterColumns.directive.js
@@ -15,23 +15,60 @@ export function LetterColumnsDirective() {
 }
 
 class LetterColumnsController {
-	constructor($scope, $log, letterColumnsModel) {
+	constructor($scope, $log) {
 		'ngInject';
 
 		this.$scope = $scope;
 		this.$log = $log;
 
-		this.lcModel = letterColumnsModel;
+		this.$log.info('constructor()', this, $scope);
+
+		this.lcModel = this.$scope.$parent.main.letterColumnsModel;
 	}
 
 	$onInit() {
 		this.$log.info('$onInit', this);
 		this.$scope.$on('$destroy', this.destroy());
+
+		this.$scope.used = this.lcModel.used;
+		this.clearSelected();
 	}
 
 	destroy() {
 		return () => {
+			this.clearSelected();
+
 			this.$log.info('destroy', this);
 		};
+	}
+
+	clearSelected() {
+		this.$scope.selected = {
+			column: -1,
+			index: -1,
+			letter: '',
+			position: -1
+		};
+	}
+
+	isClickable(pos) {
+		return this.$scope.selected.position == -1 ||
+				this.$scope.selected.position == pos &&
+				!this.$scope.used[pos];
+	}
+
+	onLetterClick(letter, column, index, pos) {
+		this.$log.info('onLetterClick()', letter, column, index);
+
+		if (this.$scope.selected.position != pos) {
+			this.$scope.selected = {
+				column: column,
+				index: index,
+				letter: letter,
+				position: pos
+			};
+		} else {
+			this.clearSelected();
+		}
 	}
 }
--- src/app/components/puzzle/letterColumns/letterColumnsModel.service.js
+++ src/app/components/puzzle/letterColumns/letterColumnsModel.service.js
@@ -11,7 +11,7 @@ export class LetterColumnsModelService {
 		// };
 
 		this.columns = [];
-		this.letters = '';
+		this.used = [];
 	}
 
 	init(letters, columnSize) {
@@ -23,20 +23,23 @@ export class LetterColumnsModelService {
 				return i % columnSize;
 			});
 
-		this.letters = letters;
-
 		this.setColumns(grouped);
 	}
 
 	setColumns(group) {
 		// angular.copy(this._.values(group), this.data.columns);
 		angular.copy(this._.values(group), this.columns);
+		angular.copy(this._.fill(Array(this.columns.length * this.columns[0].length), false), this.used);
+	}
+
+	setUsed(pos) {
+		this.used[pos] = true;
+		this.$log.info('setUsed()', pos, this);
 	}
 
 	clear() {
 		// this.data.columns = [];
 		// Clear array properly in order to update any references
 		this.columns.splice(0, this.columns.length);
-		this.letters = '';
 	}
 }
