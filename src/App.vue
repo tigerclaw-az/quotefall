835d4552d2d17d0ae6f1042e33a74a2e22e84cf5
Erik Waibel <ejwaibel+github@gmail.com>
2020-04-24 13:57:55 -0700

    Separate navigation sidebar into separate component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,6 @@
 <template>
 	<v-app>
-		<v-navigation-drawer v-model="isMenuOpen" app clipped floating stateless class="elevation-5">
-			<!-- <v-toolbar dense>
-				<v-toolbar-title>Navigation</v-toolbar-title>
-			</v-toolbar> -->
-
-			<v-list>
-				<v-list-item to="/" exact>
-					<v-list-item-icon>
-						<v-icon>move_to_inbox</v-icon>
-					</v-list-item-icon>
-					<v-list-item-title class="v-list-item-text">Home</v-list-item-title>
-				</v-list-item>
-
-				<v-list-item to="/puzzles/create">
-					<v-list-item-icon>
-						<v-icon>send</v-icon>
-					</v-list-item-icon>
-					<v-list-item-title class="v-list-item-text">Create</v-list-item-title>
-				</v-list-item>
-
-				<v-list-item>
-					<v-list-item-icon>
-						<v-icon>delete</v-icon>
-					</v-list-item-icon>
-					<v-list-item-title class="v-list-item-text">Trash</v-list-item-title>
-				</v-list-item>
-
-				<v-list-item>
-					<v-list-item-icon>
-						<v-icon>error</v-icon>
-					</v-list-item-icon>
-					<v-list-item-title class="v-list-item-text">Spam</v-list-item-title>
-				</v-list-item>
-			</v-list>
-		</v-navigation-drawer>
+		<app-sidebar></app-sidebar>
 
 		<app-top-bar></app-top-bar>
 
@@ -50,16 +16,13 @@
 <script>
 import AppTopBar from '@/components/AppTopBar.vue';
 import AppFooter from '@/components/AppFooter.vue';
+import AppSidebar from '@/components/AppSidebar.vue';
 
 export default {
 	components: {
 		AppTopBar,
 		AppFooter,
-	},
-	computed: {
-		isMenuOpen() {
-			return this.$store.state.isMenuOpen;
-		},
+		AppSidebar,
 	},
 };
 </script>
---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,40 @@
+<template>
+	<v-navigation-drawer class="elevation-5" app dark clipped floating stateless :value="isMenuOpen">
+		<!-- <v-toolbar dense>
+				<v-toolbar-title>Navigation</v-toolbar-title>
+			</v-toolbar> -->
+
+		<v-list shaped>
+			<v-list-item to="/" exact>
+				<v-list-item-icon>
+					<v-icon>archive</v-icon>
+				</v-list-item-icon>
+				<v-list-item-title class="v-list-item-text">Archives</v-list-item-title>
+			</v-list-item>
+
+			<v-list-item to="/puzzles/create" exact>
+				<v-list-item-icon>
+					<v-icon>send</v-icon>
+				</v-list-item-icon>
+				<v-list-item-title class="v-list-item-text">Create</v-list-item-title>
+			</v-list-item>
+
+			<v-list-item to="/help" exact>
+				<v-list-item-icon>
+					<v-icon>info</v-icon>
+				</v-list-item-icon>
+				<v-list-item-title class="v-list-item-text">Help</v-list-item-title>
+			</v-list-item>
+		</v-list>
+	</v-navigation-drawer>
+</template>
+
+<script>
+export default {
+	computed: {
+		isMenuOpen() {
+			return this.$store.state.isMenuOpen;
+		},
+	},
+};
+</script>
