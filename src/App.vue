94fed52ba8be65974904a03f827d45e74eb54f3d
Erik Waibel <ejwaibel+github@gmail.com>
2020-04-26 15:52:18 -0700

    Fixes placement of footer element

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
 
 		<app-top-bar></app-top-bar>
 
-		<v-content class="ml-2">
+		<v-content>
 			<v-container fluid>
 				<router-view />
-				<app-footer></app-footer>
 			</v-container>
 		</v-content>
+		<app-footer></app-footer>
 	</v-app>
 </template>
 
